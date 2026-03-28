11f41e7b9abbde8bc69a6eeee49208076887859b
Aiden Sarrafzadeh <asarrafz@uwaterloo.ca>
2026-03-28 18:48:13 -0400

    new

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/notes", (req, res) => {
+    res.send("you got 4 notes");
+});
+
 app.listen(5001, () => {
     console.log("Server is running on port 5001");
 });
