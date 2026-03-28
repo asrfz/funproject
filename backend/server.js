689c122cff9d4162850e50221f5247ab2beb9a3b
Aiden Sarrafzadeh <asarrafz@uwaterloo.ca>
2026-03-28 19:19:28 -0400

    adkfjk

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ app.get("/api/notes", (req, res) => {
     res.send("you got 4 notes");
 });
 
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message: "Note created"});
+});
+
 app.listen(5001, () => {
     console.log("Server is running on port 5001");
 });
