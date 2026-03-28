78207b1a27abe5a5cb5c42d97dd8ca1c64596043
Aiden Sarrafzadeh <asarrafz@uwaterloo.ca>
2026-03-28 19:22:39 -0400

    add delete server

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,14 @@ app.post("/api/notes", (req, res) => {
     res.status(201).json({message: "Note created"});
 });
 
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({message: "Note updated"});
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({message: "Note deleted"});
+});
+
 app.listen(5001, () => {
     console.log("Server is running on port 5001");
 });
