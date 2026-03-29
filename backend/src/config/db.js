16f1c2824c4190b0fa5894d25e8c2dc016b2ab11
Aiden Sarrafzadeh <asarrafz@uwaterloo.ca>
2026-03-28 21:06:13 -0400

    push

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try{
-        mongoose.connect("mongodb+srv://asarrafz_db_user:fsCX1JEEELESs66m@cluster0.bbc8xtv.mongodb.net/?appName=Cluster0");
+        mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("error connecting to MongoDB", error);
-        
+        process.exit(1); // exit with failure
     }
 };
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,19 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
+import dotenv from "dotenv";
 
+
+dotenv.config();
+
+const port = process.env.PORT || 5001;
 const app = express();
 
 connectDB();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
+app.listen(port, () => {
     console.log("Server is running on port 5001");
 });
 
