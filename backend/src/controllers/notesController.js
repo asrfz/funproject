cc4d06c3ac63ca5d4ac2475fa8d1bd8f1deb049b
Aiden Sarrafzadeh <asarrafz@uwaterloo.ca>
2026-03-28 22:15:45 -0400

    finish controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -24,10 +24,31 @@ export const createNote = async (req, res) => {
     }
 };
 
-export const updateNote = (req, res) => {
-    res.status(200).json({message: "Note updated"});
+export async const updateNote = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, content } = req.body;
+        const updatedNote = await Note.findByIdAndUpdate(id, { title: title, content: content }, { new: true });
+        if (!updatedNote) {
+            return res.status(404).json({message: "Note not found"});
+        }
+        res.status(200).json(updatedNote);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.error("error updating note", error);
+    }
 };
 
-export const deleteNote = (req, res) => {
-    res.status(200).json({message: "Note deleted"});
+export const deleteNote = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedNote = await Note.findByIdAndDelete(id);
+        if (!deletedNote) {
+            return res.status(404).json({message: "Note not found"});
+        }
+        res.status(200).json({message: "Note deleted"});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.error("error deleting note", error);
+    }
 };
