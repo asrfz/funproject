1e8a60e81c5cdf5163c5e7a523cb32f249a4ef3a
Aiden Sarrafzadeh <asarrafz@uwaterloo.ca>
2026-03-29 21:18:04 -0400

    add ratelims

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,11 @@
+import {Ratelimit} from "@upstash/ratelimit";
+import {Redis} from "@upstash/redis";
+
+
+//create a limiter that allows 10 requests per 20
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(process.env.UPSTASH_REDIS_REST_URL),
+    limiter: Ratelimit.slidingWindow(20, "20 s"),
+});
+
+export default ratelimit;
