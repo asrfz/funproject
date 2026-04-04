9f90eb554986868136196423c831cddd45f981c8
Aiden Sarrafzadeh <asarrafz@uwaterloo.ca>
2026-04-04 17:24:33 -0400

    Add comments to main.jsx render method
    Updated the render method to include comments for clarity.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,10 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( //take root element and put entire app into it. entire app is wrapped in react router. BrowserRouter is the navigation system that allows app to have different paths. It watches the URL and decides what page to show. Strict mode is for debugging. 
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <App /> 
       <Toaster />
     </BrowserRouter>
   </StrictMode>,
