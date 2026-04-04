38f62c3eb94445c9552a5db64a3100d6564dc001
Aiden Sarrafzadeh <asarrafz@uwaterloo.ca>
2026-04-04 14:24:32 -0400

    fix
    Added comments to clarify async/await behavior in connectDB function.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 const connectDB = async () => {
-    try{
+    try{ // await means don't continue until it happens --- async means the function takes time and therefore probably has await in it --- await means the function won't continue until this happens
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB");
     } catch (error) {
