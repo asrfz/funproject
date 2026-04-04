1990fc5c271fb98232f5d4328168a9a2415125fb
Aiden Sarrafzadeh <asarrafz@uwaterloo.ca>
2026-04-04 16:23:05 -0400

    Update rateLimiter.js

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -2,12 +2,12 @@ import ratelimit from "../config/upstash.js";
 
 const rateLimiter = async (req, res, next) => {
     try{
-        const {success} = await ratelimit.limit("my-limit-key");
+        const {success} = await ratelimit.limit("my-limit-key"); //check how many reqs made (returns a true or false)
         if(!success){
             return res.status(429).json({message: "Too many requests"});
         }
 
-        next()
+        next() //move the request onto the next part in server.js
     } catch(error){
         console.error("error in rateLimiter", error);
         next(error);
